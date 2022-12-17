cbc8ae12c87a02082d889da1e190032c14e5ec78
mayudev <99561108+mayudev@users.noreply.github.com>
2022-12-17 19:39:55 +0100

    style(search): style search results

--- packages/web/src/components/popups/search/SearchResult.tsx
+++ packages/web/src/components/popups/search/SearchResult.tsx
@@ -1,14 +1,35 @@
-import { useMap } from 'react-leaflet';
+import styled from 'styled-components';
 import { IReverse } from 'types';
 
 type Props = {
   result: IReverse;
 };
 
+const Result = styled.a`
+  display: block;
+
+  margin: 12px;
+  padding: 12px 16px;
+
+  background: ${props => props.theme.background2};
+  border-radius: 8px;
+
+  font-weight: 500;
+
+  cursor: pointer;
+  user-select: none;
+
+  transition: color 100ms ease-out;
+
+  &:hover {
+    color: ${props => props.theme.primary};
+  }
+`;
+
 export default function SearchResult({ result }: Props) {
   const showOnMap = () => {
     // TODO
   };
 
-  return <h1>{result.display_name}</h1>;
+  return <Result>{result.display_name}</Result>;
 }
